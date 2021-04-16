4d40d67eb27458f23f68ad98c6ef5f449f5ee034
mfykmn <mfykmn@gmail.com>
2021-04-16 18:23:17 +0900

    Updated

---
+++ third-party-package/spring-security/README.md
@@ -0,0 +1,3 @@
+# Spring Security
+- https://spring.pleiades.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-security
+- https://spring.io/projects/spring-security
---
+++ third-party-package/spring-security/oauth2/client-credentials/src/main/kotlin/com/example/oauth2/clientcredentials/controller/HomeController.kt
@@ -0,0 +1,13 @@
+package com.example.oauth2.clientcredentials.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HomeController {
+    @GetMapping("home")
+    fun getHome(): ResponseEntity<String> {
+        return ResponseEntity.ok("home")
+    }
+}
