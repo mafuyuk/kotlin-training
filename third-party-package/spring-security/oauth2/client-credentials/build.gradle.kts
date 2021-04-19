e572c347c17ffb889d6d9d9b2ecd1da54df62f67
mfykmn <mfykmn@gmail.com>
2021-04-19 22:34:59 +0900

    hot deploy

--- third-party-package/spring-security/oauth2/client-credentials/build.gradle.kts
+++ third-party-package/spring-security/oauth2/client-credentials/build.gradle.kts
@@ -21,6 +21,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
