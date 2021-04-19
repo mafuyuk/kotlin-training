98c19c3090dbe0ce46bb7c5b17515bef03935dcb
mfykmn <mfykmn@gmail.com>
2021-04-19 11:30:17 +0900

    Updated

--- third-party-package/spring-security/oauth2/client-credentials/build.gradle.kts
+++ third-party-package/spring-security/oauth2/client-credentials/build.gradle.kts
@@ -18,8 +18,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation ("org.springframework.security:spring-security-oauth2-resource-server")
-	implementation ("org.springframework.security.oauth:spring-security-oauth-parent:1.0.5.RELEASE")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
