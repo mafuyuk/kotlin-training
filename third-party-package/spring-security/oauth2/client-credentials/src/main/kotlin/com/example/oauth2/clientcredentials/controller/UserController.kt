cf40b4586efb39b7de0e76c21c49adfe918b3405
mfykmn <mfykmn@gmail.com>
2021-04-24 23:43:08 +0900

    Updated

--- third-party-package/spring-security/oauth2/client-credentials/src/main/kotlin/com/example/oauth2/clientcredentials/controller/UserController.kt
+++ third-party-package/spring-security/oauth2/client-credentials/src/main/kotlin/com/example/oauth2/clientcredentials/controller/UserController.kt
@@ -1,19 +1,30 @@
 package com.example.oauth2.clientcredentials.controller
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
 class UserController {
+
+    val logger: Logger = LoggerFactory.getLogger(UserController::class.java)
+
     @GetMapping("user")
     fun getUser(): ResponseEntity<String> {
         return ResponseEntity.ok("users")
     }
 
     @GetMapping("user/{id}")
-    fun getUserById(@PathVariable("id") id: Int?): ResponseEntity<Int> {
+    fun getUserById(
+        @AuthenticationPrincipal principal: Jwt,
+        @PathVariable("id") id: Int?
+    ): ResponseEntity<Int> {
+        logger.info(principal.getClaimAsString("scope")) // claimの利用
         return ResponseEntity.ok(id)
     }
 }
