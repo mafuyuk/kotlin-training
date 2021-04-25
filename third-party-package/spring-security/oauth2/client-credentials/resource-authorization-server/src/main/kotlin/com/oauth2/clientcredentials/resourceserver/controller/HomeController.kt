5d5ee036cc8c32859cf91a08774846c475ce35e6
mfykmn <mfykmn@gmail.com>
2021-04-25 17:57:57 +0900

    Update

---
+++ third-party-package/spring-security/oauth2/client-credentials/resource-authorization-server/src/main/kotlin/com/oauth2/clientcredentials/resourceserver/controller/HomeController.kt
@@ -0,0 +1,13 @@
+package com.oauth2.clientcredentials.resourceserver.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HomeController {
+    @GetMapping("home")
+    fun getHome(): ResponseEntity<String> {
+        return ResponseEntity.ok("home")
+    }
+}
