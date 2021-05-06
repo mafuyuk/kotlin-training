9ff56a6e69dfe678d6a0fb85fd6420ed759794d2
mfykmn <mfykmn@gmail.com>
2021-05-06 18:01:23 +0900

    Updated

--- third-party-package/spring-boot/webflux/README.md
+++ third-party-package/spring-boot/webflux/README.md
@@ -1,1 +1,10 @@
 # Spring WebFlux
+## 環境構築
+```bash
+user@host: ~/workspace $ ./gradlew bootRun
+```
+
+## 実行
+```bash
+user@host: ~/workspace $ curl -XGET http://localhost:8080/foo
+```
--- third-party-package/spring-boot/webflux/src/main/kotlin/com/example/webflux/Application.kt
+++ third-party-package/spring-boot/webflux/src/main/kotlin/com/example/webflux/Application.kt
@@ -2,10 +2,22 @@ package com.example.webflux
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
 
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
 	runApplication<Application>(*args)
+}
+
+@RestController
+@RequestMapping("/foo")
+class FooController() {
+
+	@GetMapping
+	fun foo() = "foo"
 }
