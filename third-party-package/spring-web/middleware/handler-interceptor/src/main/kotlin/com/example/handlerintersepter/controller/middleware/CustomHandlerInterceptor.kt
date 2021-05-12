9a8c611b1881bcb445592ed4545e44fc1891a392
mfykmn <mfykmn@gmail.com>
2021-05-12 13:18:30 +0900

    Updated

--- third-party-package/spring-web/middleware/handler-interceptor/src/main/kotlin/com/example/handlerintersepter/controller/middleware/CustomHandlerInterceptor.kt
+++ third-party-package/spring-web/middleware/handler-interceptor/src/main/kotlin/com/example/handlerintersepter/controller/middleware/CustomHandlerInterceptor.kt
@@ -3,6 +3,7 @@ package com.example.handlerintersepter.controller.middleware
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.web.servlet.HandlerInterceptor
+import java.util.*
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -11,7 +12,12 @@ class CustomHandlerInterceptor : HandlerInterceptor {
 
     @Throws(Exception::class)
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        logger.info("preHandle : {} {} {}", request, request, handler)
+        logger.info("preHandle : {}", request)
+        Arrays.stream(request.cookies).forEach { cookie ->
+            if(cookie.name == "name") {
+                logger.info(cookie.value)
+            }
+        }
         return true
     }
 }
