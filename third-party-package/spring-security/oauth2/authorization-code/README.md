bdde50c3fe71bcefb764e26cadcf89cd72a119c1
mfykmn <mfykmn@gmail.com>
2021-05-29 22:49:13 +0900

    Updated

---
+++ third-party-package/spring-security/oauth2/authorization-code/README.md
@@ -0,0 +1,9 @@
+# Authorization Code
+- [RFC6749 Authorization Code](https://tools.ietf.org/html/rfc6749#section-4.1)
+
+## 参考リンク
+### Okta
+### フローについて
+- https://www.authlete.com/ja/resources/videos/
+#### OAuth2.0
+- [OAuth 2.0 全フローの図解と動画 1. 認可コードフロー](https://qiita.com/TakahikoKawasaki/items/200951e5b5929f840a1f#1-%E8%AA%8D%E5%8F%AF%E3%82%B3%E3%83%BC%E3%83%89%E3%83%95%E3%83%AD%E3%83%BC)
---
+++ third-party-package/spring-security/oauth2/authorization-code/resource-server/src/main/kotlin/com/oauth2/authorizationcode/resourceserver/controller/HomeController.kt
@@ -0,0 +1,13 @@
+package com.oauth2.authorizationcode.resourceserver.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HomeController {
+    @GetMapping("home")
+    fun getHome(): ResponseEntity<String> {
+        return ResponseEntity.ok("home")
+    }
+}
